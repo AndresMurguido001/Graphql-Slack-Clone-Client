16f184e2d0b598fe4a897b7c53254a728cf658fe
Your Name <dre.murguido@gmail.com>
2018-07-11 07:05:05 -0600

    Comment component and fetching messages

--- src/containers/MessageContainer.js
+++ src/containers/MessageContainer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Message from '../components/Messages'
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
+import { Comment } from 'semantic-ui-react'
 
 const messagesQuery = gql`
 query($channelId:Int!){
@@ -11,12 +12,30 @@ query($channelId:Int!){
         user{
             username
         }
-        createdAt
+        created_at
     }    
 }
 `;
 
-const MessageContainer =  ({ data: { loading, messages } }) => (loading ? null : <Message>{JSON.stringify(messages)}</Message>);
+const MessageContainer =  ({ data: { loading, messages } }) => (loading ? null :
+    <Message>
+        <Comment.Group>
+            {messages.map(x => (
+            <Comment key={x.id}>
+                <Comment.Content>
+                    <Comment.Author as='a'>{x.user.username}</Comment.Author>
+                    <Comment.Metadata>
+                        <div>{x.created_at}</div>
+                    </Comment.Metadata>
+                    <Comment.Text>{x.text}</Comment.Text>
+                    <Comment.Actions>
+                        <Comment.Action>Reply</Comment.Action>
+                    </Comment.Actions>
+                </Comment.Content>
+            </Comment>
+            ))}
+        </Comment.Group>
+    </Message>);
 export default graphql(messagesQuery, {
     variables: props => ({
         channelId: props.channelId
